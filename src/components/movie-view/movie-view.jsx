e779f6024dd9d1c070241ff8001e32d8a0092dd1
jrodriguez2790 <jrodriguez62790@gmail.com>
2024-09-14 12:35:19 -0400

    updates for task 3.4
    If applied, this commit will have updated the following files to include prop-types and replacing movie arrays with an empty array.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
@@ -14,13 +16,28 @@ export const MovieView = ({ movie, onBackClick }) => {
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.genre.name}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.director.name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired
+    }).isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired
+    }).isRequired,
+    image: PropTypes.string.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
