747f3c8d4cb64128f26cb35e13cc93349d23eaf3
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-09-27 05:10:44 -0400

    adds bootstrap 'Card'
    If applied, this commit will have wrapped the content in a bootstrap 'Card' for the movie-card.jsx file.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div onClick={() => { onMovieClick(movie); }}>
-      {movie.title}
-    </div>
+    <Card onClick={() => { onMovieClick(movie); }} className="mb-3" style={{ cursor: 'pointer' }}>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+      </Card.Body>
+    </Card>
   );
 };
 
