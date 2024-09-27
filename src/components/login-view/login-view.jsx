480f9256551a926d55a4834ea38589c00d0aa923
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-09-27 05:15:42 -0400

    adds form and button components
    If applied, this commit will have used bootstrap’s form and button components to give the login-view some style.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,65 +1,63 @@
-import React, { useState } from "react"; // Add useState from React
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
-    // Prevent form from reloading the page
     event.preventDefault();
 
-    // Sending the user input to the backend API
-    const data = {
-      username: username,
-      password: password,
-    };
+    const data = { username, password };
 
     fetch("https://jar-movies-flix-9c6c1a784786.herokuapp.com/login", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Login response: ", data);
         if (data.user) {
-          // If user data is returned, log in the user
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
           onLoggedIn(data.user, data.token);
         } else {
           alert("Invalid credentials. No such user.");
         }
       })
       .catch((e) => {
-        console.error("Login error: ", e);
         alert("An error occurred during login.");
       });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
           type="text"
           value={username}
-          minLength="6" // Corrected minlength to minLength
           onChange={(e) => setUsername(e.target.value)}
           required
+          minLength="3"
         />
-      </label>
-      <label>
-        Password:
-        <input
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
           type="password"
           value={password}
-          minLength="8" // Corrected minlength to minLength
           onChange={(e) => setPassword(e.target.value)}
           required
+          minLength="8"
         />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
   );
 };
