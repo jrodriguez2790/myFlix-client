e63edad5903cd608cf38d7c8847cc031c6f2233d
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-09-27 04:50:58 -0400

    adds button and form
    If applied, this commit will have added button and form components to the signup form.

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const SignupView = ({ onSignedUp }) => {
   const [username, setUsername] = useState("");
@@ -10,17 +12,15 @@ export const SignupView = ({ onSignedUp }) => {
     event.preventDefault();
 
     const data = {
-      username: username,
-      password: password,
-      email: email,
-      birthday: birthday,
+      username,
+      password,
+      email,
+      birthday,
     };
 
     fetch("https://jar-movies-flix-9c6c1a784786.herokuapp.com/users", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     }).then((response) => {
       if (response.ok) {
@@ -33,44 +33,51 @@ export const SignupView = ({ onSignedUp }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
           type="text"
           value={username}
-          minLength="3"
           onChange={(e) => setUsername(e.target.value)}
           required
+          minLength="3"
         />
-      </label>
-      <label>
-        Password:
-        <input
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
+          minLength="8"
         />
-      </label>
-      <label>
-        Email:
-        <input
+      </Form.Group>
+
+      <Form.Group controlId="formEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-      </label>
-      <label>
-        Birthday:
-        <input
+      </Form.Group>
+
+      <Form.Group controlId="formBirthday">
+        <Form.Label>Birthday</Form.Label>
+        <Form.Control
           type="date"
           value={birthday}
           onChange={(e) => setBirthday(e.target.value)}
         />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Sign Up
+      </Button>
+    </Form>
   );
 };
