374c6747235449879b59cec7ec98175918742b55
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-10-20 12:55:34 -0400

    imports scss file
    If applied, this commit will have properly imported the login-view.scss file.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import './login-view.scss'; // Import the SCSS file
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
